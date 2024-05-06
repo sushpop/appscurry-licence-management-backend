a8fa67bf9f3d36141f8a191a97c5b5fffcea10f5
sushpop <sushant.pophli@gmail.com>
2024-05-06 10:02:07 +0100

    Cleanup authentication

--- authentication/src/index.ts
+++ authentication/src/index.ts
@@ -1,14 +1,3 @@
 import { registerUser } from './registerUser'
-// TODO: DEPRECATION
-// import { login } from './login'
 
-// TODO: DEPRECATION - POTENTIAL
-import { validateToken } from './validateToken'
 exports.registerUser = registerUser
-// exports.login = login
-exports.validateToken = validateToken
-
-// TODO: Revisit following links: 
-// https://firebase.google.com/docs/functions/organize-functions?gen=2nd#index.js
-// https://medium.com/@george43g/organise-firebase-functions-part-1-optimize-cold-start-time-cd36f76d2133
-// Try to split the functions and code in microservices like features
